031ac090db8f3663857569cee2d78c1a39feba5f
Brayden Anderson <brayden.anderson2004@gmail.com>
2024-10-07 17:33:10 -0500

    sets up front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Project from './components/Project';
+import Resource from './components/Resource';
 
 function App() {
+    // Placeholder for userId - In a real app, this would come from user authentication
+    const userId = 'your_user_id';  
+
     return (
         <Router>
             <div className="App">
@@ -11,6 +16,8 @@ function App() {
                 <Routes>
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/projects" element={<Project userId={userId} />} />
+                    <Route path="/resources" element={<Resource />} />
                 </Routes>
             </div>
         </Router>
